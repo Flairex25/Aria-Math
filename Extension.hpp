112985d3e1b1243acda88750398ac3b9459a591c
Flairex25 <axikiproductions@gmail.com>
2026-08-21 17:02:39 -0600

    Update Version constant to 2

--- Extension.hpp
+++ Extension.hpp
@@ -32,7 +32,7 @@ public:
 	Edif::Runtime Runtime;
 
 	static const int MinimumBuild = 254;
-	static const int Version = 1;
+	static const int Version = 2;
 
 	// Warning: OEFLAGS/OEPREFS cannot be freely modified when you have used them in MFAs.
 	static constexpr OEFLAGS OEFLAGS = OEFLAGS::NONE;
