0adc9808e4bbb08cd4518aa4ad3c9bf9d4b5565d
Flairex25 <axikiproductions@gmail.com>
2026-08-22 22:15:31 -0600

    Update StringArrays.hpp

--- AriaMath/StringArrays.hpp
+++ AriaMath/StringArrays.hpp
@@ -10,7 +10,7 @@
 class StringArray {
 private:
 	const TCHAR* ID = _T("[EMPTY]");
-	const TCHAR* LoopID = _T("");
+	const TCHAR* LoopID = _tcsdup(_T(""));
 	std::vector<const TCHAR*> arr;
 	int loopIndex = 0;
 	int loopAmount = 0;
@@ -44,6 +44,7 @@ public:
 		this->ID = ID;
 	}
 	void SetLoopID(const TCHAR* LoopID) {
+		free((void*)this->LoopID);
 		this->LoopID = LoopID;
 	}
 	void SetArray(std::vector<const TCHAR*> arr) {
